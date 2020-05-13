5d2bfc46e092c74be876ed01aedad56ba74fea4d
Max Petriev <maxpetriev@gmail.com>
2020-05-13 16:43:24 +0300

    add keosd to docker-compose.yml

--- src/vdexnode/eosio/docker-entrypoint.sh
+++ src/vdexnode/eosio/docker-entrypoint.sh
@@ -3,4 +3,13 @@
 set -o errexit      # make your script exit when a command fails.
 set -o nounset      # exit when your script tries to use undeclared variables.
 
-exec /usr/bin/nodeos --config /eosio/config.ini --data-dir /eosio/data --genesis-json /eosio/genesis.json --disable-replay-opts $@
+case "$1" in
+  nodeos)
+    exec /usr/bin/nodeos --config /eosio/config.ini --data-dir /eosio/data --genesis-json /eosio/genesis.json --disable-replay-opts
+    ;;
+  keosd)
+    exec /usr/bin/keosd  --wallet-dir /eosio/wallet  --http-server-address 8901
+    ;;
+  *)
+    exec "$@"
+esac

93a3dfa9654b8ae9c5493fdfe06a28fe37e0c600
Sylvain Cormier <sylvaincormier@protonmail.com>
2020-09-18 18:16:44 -0400

    - doc - -custodian

---
+++ src/vdexnode/contracts/vtxdistribut/vtxdistribut.hpp
@@ -0,0 +1,147 @@
+#pragma once
+
+#include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
+#include <eosio/symbol.hpp>
+#include <eosio/contract.hpp>
+#include <eosio/name.hpp>
+#include <vector>
+#include <string>
+
+#define VOTING_CONTRACT name("vdexdposvote")
+
+using std::string;
+using namespace eosio;
+
+class[[eosio::contract]] vtxdistribut : public eosio::contract {
+    public:
+        using contract::contract;
+        const name pool_account = name("vtxtestpool1");
+        const name treasury = name("vtxdistribut");
+        const name vtxsys_contract = name("volentixgsys");
+        const name voting_contract = name("vdexdposvote");
+        const symbol vtx_symbol = symbol(symbol_code("VTX"), 8);
+        const uint32_t one_day = 24 * 60 * 60;
+        const uint32_t daily_reward_id = 0;
+        const uint32_t standby_reward_id = 1;
+
+        vtxdistribut(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds),
+            rewards(receiver, receiver.value), usblacklist(receiver, receiver.value), 
+            rewardhistory(receiver, receiver.value), inituptime(receiver, receiver.value) {}
+         	
+        
+        [[eosio::action]]
+        void paycore();
+
+        [[eosio::action]]
+        void paycampaign(string campaign);       
+        
+        [[eosio::action]]
+        void uptime(name account, const std::vector<uint32_t> &job_ids);
+        
+        [[eosio::action]]
+        void addblacklist(name account, string ip);
+
+        [[eosio::action]]
+        void rmblacklist(name account);
+
+        [[eosio::action]]
+        void initup(name account);
+
+        [[eosio::action]]
+        void rmup(name account);
+
+        [[eosio::action]]
+        void calcrewards(uint32_t job_id);
+
+        [[eosio::action]]
+        void getreward(name node);
+
+        struct [[eosio::table]] reward_info {
+            uint32_t reward_id;
+            uint32_t reward_period;
+            asset reward_amount;
+            asset standby_amount;
+            uint32_t rank_threshold;
+            // set standby_rank_threshold to 0 to disable it
+            uint32_t standby_rank_threshold;
+            string memo;
+            string standby_memo;
+            uint64_t primary_key() const { return reward_id;}
+        };
+
+        typedef eosio::multi_index<"rewards"_n, reward_info> reward_index;
+        reward_index rewards;
+
+        [[eosio::action]]
+        void setrewardrule(const reward_info& rule);
+
+    private:
+       struct [[eosio::table]] vdexnodes_uptime {
+            uint32_t job_id;
+            uint32_t period_num;
+            uint32_t count;
+            uint32_t last_timestamp;
+            uint32_t primary_key() const { return job_id;}
+        };
+
+
+        typedef eosio::multi_index<"uptimes"_n, vdexnodes_uptime> uptime_index;
+
+        struct [[eosio::table]] blacklist {
+            name account;
+            string IP;
+            uint64_t primary_key() const { return account.value;}
+        };
+
+        typedef eosio::multi_index<"usblacklist"_n, blacklist> blacklist_index;
+        blacklist_index usblacklist;
+
+        
+        struct [[eosio::table]] init_uptime {
+            name account;
+            uint32_t init_uptime;
+            uint64_t primary_key() const { return account.value;}
+        };
+
+        typedef eosio::multi_index<"inituptime"_n, init_uptime> inituptime_index;
+        inituptime_index inituptime;
+
+        struct [[eosio::table]] reward_history {
+            uint32_t reward_id;
+            uint32_t last_timestamp;
+            uint64_t primary_key() const { return (uint64_t) reward_id;}
+        };
+
+        typedef eosio::multi_index<"rewardhistor"_n, reward_history> rewardhistory_index;
+        rewardhistory_index rewardhistory;
+
+
+        // scope by node name
+        struct [[eosio::table]] node_reward {
+            uint64_t id;
+            asset amount;
+            string memo;
+            uint64_t primary_key() const { return id;}
+        };
+        typedef eosio::multi_index<"nodereward"_n, node_reward> node_rewards;
+
+
+        void reward(name account, uint32_t job_id, uint32_t timestamp);
+        void checkblaclist ( name account );
+        void add_reward(name node, asset amount, string memo);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/vdexnode/tokens/volentixgsys/src/vdexvote.hpp
@@ -0,0 +1,116 @@
+#include <eosio/symbol.hpp>
+#include <eosio/transaction.hpp>
+#include <cmath>
+#include <eosio/asset.hpp>
+#include <eosio/eosio.hpp>
+
+using namespace eosio;
+
+class [[eosio::contract("vdexvote")]] vdexvote : public contract {
+public:
+    using contract::contract;
+
+    name vtx_account = "volentixgsys"_n;
+    std::string vtx_symbol_code = "VTX";
+    int64_t vtx_precision = 100000000;
+
+    vdexvote(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds),
+                                                                      _producers(receiver, receiver.value),
+                                                                      _voters(receiver, receiver.value) {}
+
+    [[eosio::action]]
+    void regproducer(const name producer, const std::string &producer_name,
+                     const std::string &url, const std::string &key, const std::string &node_id);
+
+    [[eosio::action]]
+    void unregprod(const name producer);
+
+    [[eosio::action]]
+    void activateprod(const name producer);
+
+    [[eosio::action]]
+    void voteproducer(const name voter_name, const std::vector <name> &producers);
+
+    [[eosio::action]]
+    void updatevotes(const name name_from, const name name_to);
+
+    using updatevotes_action = action_wrapper<"updatevotes"_n, &vdexvote::updatevotes>;
+    
+    static std::vector <name> get_voters_by_time(const name vouting_account, const uint64_t from, const uint64_t to, const name user) {
+        voters_table voters(vouting_account, vouting_account.value);
+
+        std::vector <name> voters_need;
+
+        for (auto itr = voters.begin(); itr != voters.end(); itr++) {
+            if (itr->date >= from && itr->date <= to) {
+                voters_need.push_back(itr->owner);
+            }
+        }
+
+        return voters_need;
+    }
+
+    static double get_votes(name voting_contract, name account) {
+        producers_table producers(voting_contract, voting_contract.value);
+        
+        auto iterator = producers.find(account.value);
+        
+        if (iterator != producers.end()) {
+            return iterator->total_votes;
+        } else {
+            return 0;
+        }
+    }
+
+private:
+
+    struct [[eosio::table]] producer_info {
+        name owner;
+        double total_votes = 0;
+        bool is_active = true;
+        std::string producer_name;
+        std::string url;
+        std::string key;
+        std::string node_id;
+
+        uint64_t primary_key() const { return owner.value; }
+
+        double by_votes() const { return is_active ? -total_votes : total_votes; }
+
+        bool active() const { return is_active; }
+    };
+
+    typedef eosio::multi_index<"producers"_n, producer_info,
+            indexed_by < "prototalvote"_n, const_mem_fun < producer_info, double, &producer_info::by_votes>  >
+    >
+    producers_table;
+
+    producers_table _producers;
+
+    struct [[eosio::table]] voter_info {
+        name owner;
+        std::vector <name> producers;
+        uint64_t date;
+        double votes_per_prod = 0;
+        double tokens = 0;
+
+        uint64_t primary_key() const { return owner.value; }
+    };
+
+    typedef eosio::multi_index<"voters"_n, voter_info> voters_table;
+
+    voters_table _voters;
+
+    void update_voters(const name voter_name, const std::vector <name> &producers,
+                       const double votes_per_prod, const double tokens, const uint64_t vouting_time);
+
+    void update_producers(const name voter_name, const std::vector <name> &producers,
+                          const double votes_per_prod, const bool undo_prev_vote);
+
+    void vote(const name voter_name, const std::vector <name> &producers,
+              const double balance_tokens, const uint64_t vouting_time);
+
+    double get_token_balance(const name account);
+
+    void updatevote(const name voter_name);
+};

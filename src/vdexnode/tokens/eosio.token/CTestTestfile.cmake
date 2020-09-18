8919d6818355431cc113b17d58e6c182022329f3
Sylvain Cormier <sylvaincormier@protonmail.com>
2020-09-18 13:07:55 -0400

    added tokens dirctory

---
+++ src/vdexnode/tokens/eosio.token/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sylvain/eos/contracts/eosio.token
+# Build directory: /home/sylvain/eos/build/contracts/eosio.token
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(validate_eosio.token_abi "/home/sylvain/eos/build/scripts/abi_is_json.py" "/home/sylvain/eos/contracts/eosio.token/eosio.token.abi")

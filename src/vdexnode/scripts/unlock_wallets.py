e61dbbf7d730cf6cd5502676efb00e48ee38657f
Sylvain Cormier <sylvain@volentixlabs.com>
2020-10-19 14:20:51 -0400

    wallet

--- src/vdexnode/scripts/unlock_wallets.py
+++ src/vdexnode/scripts/unlock_wallets.py
@@ -2,6 +2,7 @@
 import subprocess
 
 unlock = {
+    "eosio":"PW5KCsKVF1SGtJrPPXaTXfP49P4jMY77ih6p5RyHJ99K9NHVszHry",
     "eosio.bpay":"PW5JJ7DBT6GUxaMGMeWyxLsHGRQYwDUxMYXfJYDkewzANuCKTmLSW",
     "eosio.msig":"PW5HypaF1psJPe2xdLnVcQu6V9cwteJFdYmsbpdKCxzbThbxGRFzj",
     "eosio.names":"PW5JKUryRD6AsS7uxJSecAnm5HpqiLg1uiHFh2Wn6LhBYRSQkmZzU",

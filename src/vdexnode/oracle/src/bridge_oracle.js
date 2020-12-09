609696733f0492e7ad33a238b010c06ccd5d5955
Lohann Paterno Coutinho Ferreira <lohanncoutinho@gmail.com>
2020-12-09 20:42:31 -0300

    Remove unused variable

--- src/vdexnode/oracle/src/bridge_oracle.js
+++ src/vdexnode/oracle/src/bridge_oracle.js
@@ -1,4 +1,4 @@
-let json = require('../VTX.json')
+let json = require('./VTX.json')
 const Web3 = require('web3')
 const { Api, JsonRpc, RpcError } = require('eosjs')
 const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig')
@@ -15,105 +15,92 @@ const {
   EOS_POOL_ACCOUNT,
   EOS_ACCOUNT,
   EOS_CUSTODIAN_ACCOUNT,
-  EOS_DISTRIBUTION_ACCOUNT,
   EOS_NODE_URL,
   EOS_PRIVATE_KEY,
 } = process.env
 
-async function eth_balance() {
+async function main() {
   // CONNECT TO WEB3 PROVIDER
   const web3Provider = new Web3.providers.HttpProvider(ETH_NODE_URL)
   const web3 = new Web3(web3Provider)
   const contract = new web3.eth.Contract(json.abi, ETH_TOKEN_CONTRACT)
 
-  // WAIT FOR SYNC
+  // Checks if ethereum node is syncing
   const isSyncing = await web3.eth.isSyncing()
   if (isSyncing) {
     console.log("Ethereum chain is syncing. ABORT")
     return
   }
 
-  // Handler
+  // Setup EOS RPC
   const eos_rpc = new JsonRpc(EOS_NODE_URL, { fetch })
-  let last_eth_vtx_balance = await contract.methods.balanceOf(ETH_POOL_ADDRESS).call()
-  let eth_vtx_balance = last_eth_vtx_balance
-  let eos_vtx_balance = await eos_rpc.get_currency_balance(EOS_TOKEN_CONTRACT, EOS_POOL_ACCOUNT, 'VTX')
-  console.log('ETH VTX balance: ', eth_vtx_balance)
-  console.log('EOS VTX balance: ', eos_vtx_balance)
-  console.log('Listening...')
-  await sleep(3000)
 
+  // Updates EOS balance
+  async function update_balance() {
+    // Retrieve ETH_POOL_ADDRESS VTX balance
+    let eth_vtx_balance = await contract.methods.balanceOf(ETH_POOL_ADDRESS).call()
+    console.log('ETH VTX balance: ', eth_vtx_balance)
 
-  while (true) {
-    try {
-      // Retrieve ETH_POOL_ADDRESS VTX balance
-      eth_vtx_balance = await contract.methods.balanceOf(ETH_POOL_ADDRESS).call()
-      // eth_vtx_balance = new BigInt(last_eth_vtx_balance)
+    // Retrieve EOS VTX Balance
+    let eos_vtx_balance = await eos_rpc.get_currency_balance(EOS_TOKEN_CONTRACT, EOS_POOL_ACCOUNT, 'VTX')
+    console.log('EOS VTX balance: ', eos_vtx_balance)
 
-      console.log('ETH VTX balance: ', eth_vtx_balance)
-
-      // update balance
-      send_balance_EOS(eth_vtx_balance)
-      last_eth_vtx_balance = eth_vtx_balance
-
-      // Retrieve EOS VTX Balance
-      eos_vtx_balance = await eos_rpc.get_currency_balance(EOS_TOKEN_CONTRACT, EOS_POOL_ACCOUNT, 'VTX')
-      console.log('EOS VTX balance: ', eos_vtx_balance)
-      await sleep(5000)
-    } catch (err) {
-      console.error(err)
-    }
+    // Update balance
+    await send_balance_EOS(eth_vtx_balance)
   }
-  console.log('Stopping Bridge Oracle...')
+
+  // Execute update_balance every 3 seconds
+  function callback() {
+    setTimeout(() => {
+      update_balance()
+        .then(callback, (error) => {
+          console.error(error)
+          callback()
+        })
+    }, 3000)
+  }
+  callback()
+  console.log('Listening...')
 }
 
 async function send_balance_EOS(balance) {
   console.log(`SEND BALANCE "${balance}"\n`)
-  try {
-    const signatureProvider = new JsSignatureProvider([EOS_PRIVATE_KEY])
-    const rpc = new JsonRpc(EOS_NODE_URL, {
-      fetch,
-    })
-    const api = new Api({
-      rpc,
-      signatureProvider,
-      textDecoder: new TextDecoder(),
-      textEncoder: new TextEncoder(),
-    })
-    const timestamp = Date.now()
-    const info = await rpc.get_info()
-    console.log(info.chain_id)
-    const result = await api.transact({
-      actions: [
-        {
-          account: EOS_CUSTODIAN_ACCOUNT,
-          name: 'updtblnc',
-          authorization: [
-            {
-              actor: EOS_ACCOUNT,
-              permission: 'active',
-            },
-          ],
-          data: {
-            account: EOS_ACCOUNT,
-            balance: balance,
-            timestamp: timestamp,
+  const signatureProvider = new JsSignatureProvider([EOS_PRIVATE_KEY])
+  const rpc = new JsonRpc(EOS_NODE_URL, {
+    fetch,
+  })
+  const api = new Api({
+    rpc,
+    signatureProvider,
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder(),
+  })
+  const timestamp = Date.now()
+  const info = await rpc.get_info()
+  console.log(info.chain_id)
+  const result = await api.transact({
+    actions: [
+      {
+        account: EOS_CUSTODIAN_ACCOUNT,
+        name: 'updtblnc',
+        authorization: [
+          {
+            actor: EOS_ACCOUNT,
+            permission: 'active',
           },
+        ],
+        data: {
+          account: EOS_ACCOUNT,
+          balance: balance,
+          timestamp: timestamp,
         },
-      ],
-    }, {
-      blocksBehind: 3,
-      expireSeconds: 30,
-    })
-    console.dir(result)
-  } catch (err) {
-    console.log(err)
-    return
-  }
+      },
+    ],
+  }, {
+    blocksBehind: 3,
+    expireSeconds: 30,
+  })
+  console.dir(result)
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-eth_balance()
+main()

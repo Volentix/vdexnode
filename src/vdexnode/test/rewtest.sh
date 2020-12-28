26bb10ddf4f324d8938c6191a84d94c57af13f1b
sylvaincormier <sylvain@volentixlabs.com>
2020-12-28 19:16:26 +0000

    rewards test - modif

--- src/vdexnode/test/rewtest.sh
+++ src/vdexnode/test/rewtest.sh
@@ -1,3 +1,5 @@
+sh setrewardruleon.sh 
+sleep 10
 PAYLOAD='{"account":"v11111111111","job_ids":[1,2],"node_id":1,"memo":"1"}'
 cleos --url  http://140.82.56.143:8888 push action vistribution uptime $PAYLOAD -p v11111111111@active
-
+sh setrewardruleoff.sh 
